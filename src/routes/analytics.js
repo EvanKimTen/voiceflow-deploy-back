30bf0ee9d6ff5cfd6f08d0f2b62fa524a886092e
userNeal <117126139+EvanKimTen@users.noreply.github.com>
2023-09-27 01:03:09 -0400

    fetch added

--- src/routes/analytics.js
+++ src/routes/analytics.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const getUserKeys = require("../middleware/getUserKeys");
-
+const fetch = require("node-fetch");
 
 // Define a route to handle the specific request
 router.post("/api/proxy/top_intents/:userId", async (req, res) => {
